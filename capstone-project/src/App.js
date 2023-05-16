845e0f75a28175b733e25e865eb8747b83501237
Juan Jose Farina <juanjosefarina.jjf@gmail.com>
2023-05-16 09:30:22 -0300

    final del estilo css

--- capstone-project/src/App.js
+++ capstone-project/src/App.js
@@ -1,6 +1,6 @@
 import './App.css'
 import { Header } from './components/Header.js'
-import { Main } from './components/Main.js'
+import Main from './components/Main.js'
 import { Footer } from './components/Footer.js'
 
 function App() {
---
+++ capstone-project/src/components/FoodCard.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const FoodCard = (props) => {
+    return (
+        <div className="foodCard">
+            <img src={props.image} alt={props.imgText} className="foodImages" />
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-8">
+                        <h6 style={{fontSize:'1rem', fontWeight:'900'}}>{props.imgText}</h6>
+                    </div>
+                    <div className="col-4">
+                        <p style={{fontSize:'0.8rem', color:'#ee9972'}}>{props.price}</p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-12">
+                        <p style={{fontSize:'0.6rem'}}>{props.desc}</p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-12">
+                        <p style={{fontSize:'0.6rem', fontWeight: '900'}}>Order a delivery <i class="fas fa-shipping-fast"></i></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default FoodCard;
