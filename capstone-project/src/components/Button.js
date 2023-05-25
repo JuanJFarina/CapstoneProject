33fa54be1d2f3d57dbaf8781ffade54c03ed4739
Juan Jose Farina <juanjosefarina.jjf@gmail.com>
2023-05-25 12:38:08 -0300

    Updates

--- capstone-project/src/components/Button.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Button = (props) => {
-    return (
-      <div className="button clickable" onClick={props.handler}>{props.text}</div>
-    );
-}
-
-export default Button;
--- capstone-project/src/pages/ConfirmedBooking.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function ConfirmedBooking() {
-  return (
-    <div style={{textAlign:'center', backgroundColor:'#edefee', fontWeight: '700'}}>Confirmed Booking !</div>
-  )
-}
